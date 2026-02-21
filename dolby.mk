15eb355871e606b73ee235cac6aa63ec1e38e4f1
Garten <brilliangarten@gmail.com>
2026-02-21 13:31:23 +0700

    Update dolby.mk

--- dolby.mk
+++ dolby.mk
@@ -28,7 +28,7 @@ AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(DOLBY_PATH)/sepolicy/vendor
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/dolby_framework_matrix.xml
+# DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DOLBY_PATH)/dolby_framework_matrix.xml
 
 PRODUCT_PACKAGES += \
     vendor.dolby.hardware.dms@2.0-service.xml \
